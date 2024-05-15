d0bfdf8747f5e2426db542fe1644da0692d6c2f5
JustinJLeopard <150853780+JustinJLeopard@users.noreply.github.com>
2024-05-15 11:37:24 -0400

    Time: 45 ms (76.60%), Space: 17 MB (64.63%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,19 +1,22 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
+        return Counter(s) == Counter(t)
         
-        countS, countT = {}, {} # This creates 2 hashmaps
+    
+#         if len(s) != len(t):
+#             return False
         
-        for i in range(len(s)): # Using index i to loop through both strings b/c same length
-            countS[s[i]] = 1 + countS.get(s[i], 0) # Counting occurance of each character in string s
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-            # Each character would be string s or t at index i, thats the key of the hashmap
-            # Then increment
-            ## Key error gets thrown with countS[s[i]] = 1 + countS[s[i]] due to character does not exist in hashmap yet
-            ## the get method allows us to return a default value of 0 as a fix
-        for c in countS: # Now iterate through the hashmaps and make sure the counts are the same
-            if countS[c] != countT.get(c, 0):
-                return False
+#         countS, countT = {}, {} # This creates 2 hashmaps
+        
+#         for i in range(len(s)): # Using index i to loop through both strings b/c same length
+#             countS[s[i]] = 1 + countS.get(s[i], 0) # Counting occurance of each character in string s
+#             countT[t[i]] = 1 + countT.get(t[i], 0)
+#             # Each character would be string s or t at index i as the key of the hashmap
+#             # Then increment
+#             ## Key error gets thrown with countS[s[i]] = 1 + countS[s[i]] due to character does not exist in hashmap yet
+#             ## the get method allows us to return a default value of 0 as a fix
+#         for c in countS: # Now iterate through the hashmaps and make sure the counts are the same
+#             if countS[c] != countT.get(c, 0):
+#                 return False
             
-        return True
+#         return True
